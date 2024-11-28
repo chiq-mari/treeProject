45427ac25ea9326bc41213d5f90155bc3541277d
marielvcmr <marielvmavarez@gmail.com>
2024-11-27 22:09:50 -0400

    bugfix: hasChild function returning value

--- src/treeKingdom/peopleKingdom/main.cpp
+++ src/treeKingdom/peopleKingdom/main.cpp
@@ -6,13 +6,13 @@ int main()
 {
     Person people1;
     people1 = {30355203, "Marielv", "Mavarez", "female", 20, 7831768, 0, 0, 0};
-    Person people2{30392967, "Mariangeles", "Mavarez", "female", 21, 7831758, 1, 1, 1};
-    Person people3{7831758, "Ana", "Rangel", "female", 56, 3565988, 0, 1, 0};
+    Person people2{30392967, "Mariangeles", "Mavarez", "female", 21, 7831768, 1, 1, 1};
+    Person people3{7831768, "Ana", "Rangel", "female", 56, 3565988, 0, 1, 0};
     Person people4{7831800, "Amy", "Rochester", "male", 60, 4000000, 1, 1, 0};
     people1.printWholePerson();
     people2.printWholePerson();
     people3.printWholePerson();
     people4.printWholePerson();
 
-    people3.firstborn(people3, people4).printPerson();
+    cout<<people2.hasFather(people3);
 }
